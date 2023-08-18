540f131efb03d00ff87e81a50304af49436d650e
Jorge Sanchez <141360448+jorgesanchezdata@users.noreply.github.com>
2023-08-18 18:22:20 -0400

    Update queries.sql

--- ReelFlix/queries.sql
+++ ReelFlix/queries.sql
@@ -1,5 +1,5 @@
 -- 1. access different prices
-SELECT DISTINCT amount
+SELECT amount
 FROM payment
 ORDER by amount DESC
 LIMIT 10
@@ -20,3 +20,28 @@ SELECT COUNT(*) FROM film
 -- 5. How many distinct last names of the customers are there?
 SELECT COUNT(DISTINCT last_name) 
 FROM customer
+
+-- 6. How many payments were made by the customer with customer_id = 100?
+SELECT COUNT(*)
+FROM payment
+WHERE customer_id = 100
+
+-- 7. What is the last name of our customer with first name 'ERICA'?
+SELECT first_name
+FROM customer
+WHERE first_name = 'ERICA'
+
+-- 8. How many rentals have not been returned yet
+SELECT COUNT(*)
+FROM rental
+WHERE return_date is null
+
+--9. List of all the payment_ids with an amount less than or equal to $2.
+SELECT payment_id, amount
+FROM payment
+WHERE amount <= 2
+
+
+
+
+
