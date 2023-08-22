d84c1ba08d621af4121fcbc490bdfb4bc3c5499b
Jorge Sanchez <141360448+jorgesanchezdata@users.noreply.github.com>
2023-08-22 17:25:34 -0400

    Update queries.sql

--- ReelFlix/queries.sql
+++ ReelFlix/queries.sql
@@ -139,8 +139,14 @@ SELECT customer_id, return_date-rental_date as rental_duration
 FROM rental
 WHERE customer_id = 35
 
--- 26. Also you need to find out for the suppcity team which customer has the longest average rental duration.
+-- 26. Also, you need to find out for the suppcity team which customer has the longest average rental duration.
 SELECT customer_id, AVG(return_date-rental_date) as rental_duration
 FROM rental
 GROUP BY customer_id 
 ORDER BY rental_duration DESC
+
+-- 27. create a list of the films including the relation of rental rate/replacement cost where the rental rate is less than 4% of the replacement cost.
+SELECT film_id, ROUND(rental_rate / replacement_cost * 100, 2) as percentage
+FROM film
+WHERE ROUND(rental_rate / replacement_cost * 100, 2) < 4
+ORDER BY 2 ASC
