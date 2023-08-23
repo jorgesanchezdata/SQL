e3d4163f9c22f24de24bcb58ecc42017a86f5a45
Jorge Sanchez <141360448+jorgesanchezdata@users.noreply.github.com>
2023-08-23 17:45:37 -0400

    Update query.sql

--- AirFly/query.sql
+++ AirFly/query.sql
@@ -53,3 +53,34 @@ AND b.flight_id = t.flight_id
 GROUP BY seat_no
 ORDER by 2 DESC
 
+--6. Which passenger (passenger_name) has spent most amount in their bookings (total_amount)?
+      
+SELECT passenger_name,SUM(total_amount) FROM tickets t
+INNER JOIN bookings b
+ON t.book_ref=b.book_ref
+GROUP BY passenger_name
+ORDER BY SUM(total_amount) DESC
+
+--7. Which fare_condition has ALEKSANDR IVANOV used the most?
+      
+SELECT passenger_name, fare_conditions, COUNT(*) FROM tickets t
+INNER JOIN bookings b
+ON t.book_ref=b.book_ref
+INNER JOIN ticket_flights tf
+ON t.ticket_no=tf.ticket_no
+WHERE passenger_name = 'ALEKSANDR IVANOV'
+GROUP BY fare_conditions, passenger_name
+
+--8. Which title has GEORGE LINTON rented the most often?
+      
+SELECT first_name, last_name, title, COUNT(*)
+FROM customer cu
+INNER JOIN rental re
+ON cu.customer_id = re.customer_id
+INNER JOIN inventory inv
+ON inv.inventory_id=re.inventory_id
+INNER JOIN film fi
+ON fi.film_id = inv.film_id
+WHERE first_name='GEORGE' and last_name='LINTON'
+GROUP BY title, first_name, last_name
+ORDER BY 4 DESC
